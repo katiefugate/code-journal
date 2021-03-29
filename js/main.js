aa7610c6e79a8d6307391abca463c65b8d995a94
katiefugate <katiiierochelle@yahoo.com>
2021-03-29 16:54:26 -0700

    listen for input, change image preview.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 /* global data */
 /* exported data */
+
+var photoUrl = document.querySelector('#url');
+var img = document.querySelector('.placeholder');
+
+function photoUrlHandler(event) {
+  img.setAttribute('src', photoUrl.value);
+}
+
+photoUrl.addEventListener('input', photoUrlHandler);

42eca2ac3572489003dc402f07cc7b29f0b6d07b
katiefugate <katiiierochelle@yahoo.com>
2021-03-30 12:35:04 -0700

    defined function for DOM tree for journal entry and added loop for DOMContentLoaded event listener.

--- js/main.js
+++ js/main.js
@@ -25,3 +25,39 @@ function saveButtonHandler(event) {
 photoUrl.addEventListener('input', photoUrlHandler);
 
 form.addEventListener('submit', saveButtonHandler);
+
+function addEntry(entry) {
+  var ul = document.querySelector('.entries');
+  var entryLi = document.createElement('li');
+  var row = document.createElement('div');
+  var columnHalf = document.createElement('div');
+  var img = document.createElement('img');
+  var columnHalf2 = document.createElement('div');
+  var titleH2 = document.createElement('h2');
+  var notesP = document.createElement('p');
+  ul.appendChild(entryLi);
+  entryLi.appendChild(row);
+  row.appendChild(columnHalf);
+  columnHalf.appendChild(img);
+  row.appendChild(columnHalf2);
+  columnHalf2.appendChild(titleH2);
+  columnHalf2.appendChild(notesP);
+  ul.className = 'entries';
+  entryLi.className = 'entry';
+  row.className = 'row';
+  columnHalf.className = 'column-half';
+  columnHalf2.className = 'column-half';
+  titleH2.className = 'title-h2';
+  notesP.className = 'notes-p';
+  img.setAttribute('src', entry.url);
+  titleH2.textContent = entry.title;
+  notesP.textContent = entry.notes;
+}
+
+function contentLoadHandler(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    addEntry(data.entries[i]);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', contentLoadHandler);

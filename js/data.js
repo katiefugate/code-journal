8757dabbc53067703bf9f608a29f5fa67664bdd8
katiefugate <katiiierochelle@yahoo.com>
2021-03-29 20:28:39 -0700

    verified save to local storage on reload and each entryId incrementing.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,16 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var myStorage = window.localStorage;
+var previousDataJSON = myStorage.getItem('cj-entry-form');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+function store(event) {
+  var dataJSON = JSON.stringify(data);
+  myStorage.setItem('cj-entry-form', dataJSON);
+}
+
+window.addEventListener('beforeunload', store);
